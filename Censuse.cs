0c734bf08d7bd8acc2e40d4823ed8d308110a531
JOY <joybosu@gmail.com>
2022-03-21 22:04:45 +0530

    [Joy] add Censuse Program file

---
+++ Censuse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianCenuseStateAnalyzer
+{
+    internal class Censuse
+    {
+        public int serialNumber;
+        public string stateName;
+        public string state;
+        public int tin;
+        public string stateCode;
+        public long population;
+        public long density;
+        public long area;
+        public long housingunit;
+        public double totalArea;
+        public long waterArea;
+        public double landArea;
+            public long populationDensity;
+        public double housingDensity;
+
+        public Censuse(StateCode statecode)
+        {
+            this.serialNumber = statecode.serialNumber;
+            this.stateName=statecode.stateName;
+            this.tin = statecode.tin;
+            this.stateCode=statecode.stateCode;
+        }
+        public Censuse(CensuseData censuseData)
+        {
+            this.state = censuseData.state;
+            this.population = censuseData.population;
+            this.area = censuseData.area;
+            this.density = censuseData.density;
+
+        }
+
+    }
+}
