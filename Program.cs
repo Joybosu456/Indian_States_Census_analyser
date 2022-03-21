622809f1961008f91060f9e56b465c2f2c64ebc8
JOY <joybosu@gmail.com>
2022-03-21 22:00:06 +0530

    [Joy]Welcome to my State Analyer Problem

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace IndianCenuseStateAnalyzer
+{
+    class Peogram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my state Program Problem");
+        }
+    }
+}
