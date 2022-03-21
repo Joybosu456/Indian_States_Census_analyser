5f00541b9255b3b77a7fba9ec045319c755003fb
JOY <joybosu@gmail.com>
2022-03-21 22:19:22 +0530

    [Joy] add State Code Program file

---
+++ StateCode.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianCenuseStateAnalyzer
+{
+    public class StateCode
+    {
+        public int serialNumber;
+        public string stateName;
+        public string tin;
+        public string stateCode;
+        public StateCode(string v1,string v2,string v3,string v4)
+        {
+            serialNumber = Convert.ToInt32(v1);
+            stateName = v2;
+            tin = Convert.ToString(v3);
+            stateCode = v4;
+        }
+
+    }
+}
