71f81a9e912f2c4dd7512ddeb8c8f781e2ca0ec7
JOY <joybosu@gmail.com>
2022-03-21 22:07:51 +0530

    [Joy] add Reader program file

---
+++ CSVDataReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianCenuseStateAnalyzer
+{
+    public class CSVDataReader
+    {
+        public Censuse readfromcsv(string path,string country)
+        {
+            if(country=="india")
+            {
+
+            }
+        }
+
+    }
+}
