d2ede27bd81ccd6bac6dbb0c6823d73dd23ca2c5
JOY <joybosu@gmail.com>
2022-03-21 22:18:08 +0530

    [Joy] Add Analyer Program file

---
+++ CensuseAnalyzer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianCenuseStateAnalyzer
+{
+    public class CensuseAnalyzer
+    {
+        public enum Country
+        {
+            INDIA,BRAZIL,US,ARGENTINA,VUTAN
+        }
+        Dictionary<String, Censuse> dataMap;
+
+        public Dictionary<String, Censuse> LoadCensusDataMap(Country country, string csvfilePath, string dataheaders)
+        {
+            CSVDataReader cSVDataReader = new CSVDataReader();
+            cSVDataReader.readfromcsv(csvfilePath, country);
+        }
+        
+
+    }
+}
